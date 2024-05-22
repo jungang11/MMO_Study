c96a9ca2122af10598cc34dcec64d13050add840
jungang11 <94428775+jungang11@users.noreply.github.com>
2024-05-23 02:11:23 +0900

    feat : Player Move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,24 +4,31 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private float moveSpeed = 10.0f;
+
     void Awake()
     {
         Managers mg = Managers.Instance;
     }
 
-    // GameObject (Player)
-        // Transform
-        // PlayerController (*)
-
     void Update()
     {
+        // Local -> World
+        // TransfromDirection
+
+        // World -> Local
+        // InverseTransfromDirection
+
+        // 혹은 transform.Translate(Vector3 direction) : transform의 Local 기준으로 이동
+
         if(Input.GetKey(KeyCode.W))
-            transform.position += new Vector3(0f, 0f, 1.0f);
+            transform.position += transform.TransformDirection(Vector3.forward * moveSpeed * Time.deltaTime);
         if(Input.GetKey(KeyCode.S))
-            transform.position += new Vector3(0f, 0f, -1.0f);
+            transform.position += transform.TransformDirection(Vector3.back * moveSpeed * Time.deltaTime);
         if(Input.GetKey(KeyCode.A))
-            transform.position += new Vector3(1.0f, 0f, 0f);
+            transform.position += transform.TransformDirection(Vector3.left * moveSpeed * Time.deltaTime);
         if(Input.GetKey(KeyCode.D))
-            transform.position += new Vector3(-1.0f, 0f, 0f);
+            transform.position += transform.TransformDirection(Vector3.right * moveSpeed * Time.deltaTime);
     }
 }
